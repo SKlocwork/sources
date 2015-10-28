6d4509c54a1ada4aa83bed0e499f3345c603db7e
SKlocwork <Sergey.Kochetkov@roguewave.com>
2015-10-28 10:28:25 -0400

    Update gzclose.c
    ttt

--- zlib-1.2.8/gzclose.c
+++ zlib-1.2.8/gzclose.c
@@ -19,7 +19,8 @@ int ZEXPORT gzclose(file)
         
     int buf[5];
     buf[5] = 1;
-    
+    int buff[5];
+    buff[5] = 1;
     state = (gz_statep)file;
 
     return state->mode == GZ_READ ? gzclose_r(file) : gzclose_w(file);
